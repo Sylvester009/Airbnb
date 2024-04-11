563d3310450db48f369af16edc3a0e46d4a514e5
Sylvester009 <sasy260902@gmail.com>
2024-04-11 15:40:49 +0100

    Navbar UI

---
+++ app/components/Container.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+interface ContainerProps {
+    children: React.ReactNode;
+}
+
+const Container: React.FC<ContainerProps> = ({
+    children
+}) => {
+    return (
+<div className="">
+    {children}
+    </div>
+    );
+};
+
+export default Container;
---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import Container from "../Container";
+
+const Navbar = () => {
+    return (
+        <div className="w-full bg-white z-10 fixed shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>
+                    anything
+                </Container>
+                </div>
+        </div>
+    );
+};
+
+export default Navbar;
