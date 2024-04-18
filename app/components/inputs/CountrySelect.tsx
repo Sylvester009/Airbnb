9fac6bb6b61e9cfcd156eb6b850d4667274ae08a
Sylvester009 <sasy260902@gmail.com>
2024-04-18 21:41:45 +0100

    location completion

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -29,7 +29,7 @@ const CountrySelect:React.FC<CountrySelectProps> = ({
         isClearable
         options={getAll()}
         value={value}
-        onChange={(value)=> onChange(value as CountrySelectValue)}
+        onChange={(value: CountrySelectValue) => onChange(value as CountrySelectValue)}
         formatOptionLabel={(option:any)=> (
           <div className='flex flex-row items-center gap-3'>
             <div>{option.flag}</div>
@@ -46,7 +46,7 @@ const CountrySelect:React.FC<CountrySelectProps> = ({
           input: () => 'text-lg',
           option: () => 'text-lg'
         }}
-        theme={(theme) => ({
+        theme={(theme: { colors: any; }) => ({
           ...theme,
           borderRadius: 6,
           colors: {
